b6d540f9f49451ce382a4387376a61d03afc5c3e
Hilder Santos <hilder@hildersantos.com>
2018-09-02 15:53:44 -0300

    Referências de usuários

--- src/index.js
+++ src/index.js
@@ -12,19 +12,29 @@ const Todo = types
   .model({
     id: types.identifierNumber,
     name: types.optional(types.string, ""), // default values
-    done: types.optional(types.boolean, false)
+    done: types.optional(types.boolean, false),
+    user: types.maybe(types.reference(types.late(() => User)))
   })
   .actions(self => {
     function setName(newName) {
       self.name = newName;
     }
+    function setUser(user) {
+      if (user === "") {
+        // When selected is empty
+        self.user = null;
+      } else {
+        self.user = user;
+      }
+    }
     function toggle() {
       self.done = !self.done;
     }
-    return { setName, toggle };
+    return { setName, setUser, toggle };
   });
 
 const User = types.model({
+  id: types.identifierNumber,
   name: types.optional(types.string, "")
 });
 
@@ -53,15 +63,42 @@ const RootStore = types
   });
 
 const store = RootStore.create({
-  users: {}, // Users não está marcado como opcional. Interessante!
+  users: {
+    1: {
+      id: 1,
+      name: "mweststrate"
+    },
+    2: {
+      id: 2,
+      name: "mattiamanzati"
+    },
+    3: {
+      id: 3,
+      name: "johndoe"
+    }
+  },
   todos: {
     1: {
-      id: id,
+      id: 1,
       name: "Eat a cake",
       done: true
     }
   }
 });
+
+const UserPickView = observer(props => (
+  <select
+    value={props.user ? props.user.id : ""}
+    onChange={e => props.onChange(e.currentTarget.value)}
+  >
+    <option value="">--none--</option>
+    {values(props.store.users).map(user => (
+      <option value={user.id} key={user.id}>
+        {user.name}
+      </option>
+    ))}
+  </select>
+));
 
 const TodoView = observer(props => {
   return (
@@ -76,11 +113,16 @@ const TodoView = observer(props => {
         value={props.todo.name}
         onChange={e => props.todo.setName(e.currentTarget.value)}
       />
+      <UserPickView
+        user={props.todo.user}
+        store={props.store}
+        onChange={userId => props.todo.setUser(userId)}
+      />
     </div>
   );
 });
 
-const TodoViewCounter = observer(props => (
+const TodoCounterView = observer(props => (
   <div>
     {props.store.pendingCount} pending, {props.store.completedCount} completed
   </div>
@@ -93,12 +135,12 @@ const AppView = observer(props => {
         Add Task
       </button>
       {values(props.store.todos).map(todo => (
-        <TodoView key={todo.id} todo={todo} />
+        <TodoView key={todo.id} store={props.store} todo={todo} />
       ))}
-      <TodoViewCounter store={props.store} />
+      <TodoCounterView store={props.store} />
       <h4>Completadas</h4>
       {props.store.getTodosWhereDoneIs(true).map(todo => (
-        <TodoView key={todo.id} todo={todo} />
+        <TodoView key={todo.id} store={props.store} todo={todo} />
       ))}
     </div>
   );
