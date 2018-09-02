c42d553c9b0a79b6cd399261aa4d19813434bc3a
Hilder Santos <hilder@hildersantos.com>
2018-09-02 14:06:42 -0300

    Mudança nos types

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import { types } from "mobx-state-tree";
 
+// Criando os modelos
 const Todo = types.model({
-  name: "", // default values
-  done: false
+  name: types.optional(types.string, ""), // default values
+  done: types.optional(types.boolean, false)
 });
 
 const User = types.model({
-  name: ""
+  name: types.optional(types.string, "")
 });
 
 // Crio as instâncias com os valores padrão.
 const john = User.create();
 const eat = Todo.create();
 
+// Sobrescrevendo os valores padrão
+const sleep = Todo.create({ name: "Sleep" });
+
 console.log("John:", john.toJSON());
 console.log("Eat TODO:", eat.toJSON());
+console.log("Sleep TODO:", sleep.toJSON());
